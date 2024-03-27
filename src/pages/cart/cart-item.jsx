95a7f3202421bc9605e80ceaf3c15d2a1d45850d
Paarth Arora <paarth.arora21@gmail.com>
2024-03-26 21:05:41 -0400

    Added new method to calculate total cart amount in Shop Context and update amount of item for each product. Imported items into cart and added ability to display subtotal, added button to continue shopping and logic to only display checkout, continue shopping when subtotal is greater than 0

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from '../../context/shop-context'
 
 export const CartItem = (props) => {
     const { id, productName, price, productImage } = props.data
-    const { cartItems, addToCart, removeFromCart } = useContext(ShopContext)
+    const { cartItems, addToCart, removeFromCart, updateCartItemCount } = useContext(ShopContext)
     return (
         <div className='cartItem'>
             <img src={productImage} />
@@ -14,7 +14,7 @@ export const CartItem = (props) => {
                 <p>${price}</p>
                 <div className='countHandler'>
                     <button onClick={() => addToCart(id)}>+</button>
-                    <input value={cartItems[id]} />
+                    <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} />
                     <button onClick={() => removeFromCart(id)}>-</button>
                 </div>
 
